94db9d6945a052d3bfa43d79b5f2fc8b70a2b2e5
Dev-Laurin <lafisher@alaska.edu>
2019-02-24 15:37:51 -0900

    Added ideas, thoughts, on how to begin.

---
+++ assistant.py
@@ -0,0 +1,19 @@
+class Assistant: 
+	#Variables 
+	self.name = 'Jarvis'
+	
+	#3d model animations 
+
+	#
+
+	#Constructor
+	#def __init__(self):
+
+	#Member Functions
+	def setName(self, name):
+		self.name = name 
+
+	def getName(self):
+		return self.name
+
+
